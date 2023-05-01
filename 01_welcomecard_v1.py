6a389e354514902992d00de05d3b7f297c448764
甄耀东 <zhent@middleton.school.nz>
2023-05-02 10:01:34 +1200

    update

---
+++ 01_welcomecard_v1.py
@@ -0,0 +1,13 @@
+"""The Welcome Card V1
+The welcome card is to welcome the user to the program
+It will be formatted in a way that will be appealing to the user"""
+
+welcome = "Welcome to the Maori Month Quiz"
+top_bottom = "*" * len(welcome)  # For formatting the welcome statement
+print(f"{top_bottom}\n"
+      f"{welcome}\n"
+      f"{top_bottom}")
+print()
+name = input("What is your name? ")  # Ask for the user's name
+print(f"{top_bottom}")
+age = int(input("What is your age? "))  # Ask for the user's age
