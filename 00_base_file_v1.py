ac2db140c2ce199d08fc9a4c54c8ccd4216c29f3
甄耀东 <zhent@middleton.school.nz>
2023-05-11 11:36:10 +1200

    instructions v1

---
+++ 00_base_file_v1.py
@@ -0,0 +1,2 @@
+"""00_base_file_v1.py
+This is the base file where all the components will be compiled into a program"""
---
+++ 03_instructions_v1.py
@@ -0,0 +1,18 @@
+"""03_instructions_v1.py
+This is the component for the instructions
+for teaching the user how this program works"""
+
+# The instructions for the quiz
+instructions = print("When the quiz begins, a question on the Maori Months will be shown.\n"
+                     "Then enter the answer to the question.\n"
+                     "If you get all of the questions right, you will get a prize!\n"
+                     "Good Luck.")
+loop = ""
+while loop != "x":
+    Continue = input("Press <Enter> to continue")
+    if Continue == "":
+        print("program continues")
+        loop = "x"
+    else:
+        print("Please just press enter")
+
