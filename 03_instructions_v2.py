1372e6d28cbdb34a8b11ef747aac67b9e6e0c8d2
甄耀东 <zhent@middleton.school.nz>
2023-05-12 14:35:36 +1200

    finalized instructions

--- 03_instructions_v2.py
+++ 03_instructions_v2.py
@@ -3,16 +3,25 @@ Turns v1 into a function"""
 
 
 # The Function
-def instructions():
+def instructions(proceed_text):
     instructions_title = "How to play"
     print("*" * len(instructions_title))
     print(instructions_title)
     print("*" * len(instructions_title))
+    # The instructions
     print("When the quiz begins, a question on the Maori Months will be shown.\n"
           "Then enter the answer to the question.\n"
           "If you get all of the questions right, you will get a prize!\n"
           "Good Luck.")
+    # Asks the user to press the Enter Key to continue
+
+    while True:
+        proceed = input(proceed_text)
+        if proceed != "":
+            print("Invalid input. Please try again.")
+        else:
+            break
 
 
 # Main Routine
-instructions()
+instructions("Press <Enter> to continue")
